42093e330868540100742a78efcad1b6657938e7
hehongliang <hehongliang@yidian-inc.com>
2019-12-26 22:25:52 +0800

    add example op

---
+++ tef/tef/core/kernels/kernel_example.h
@@ -0,0 +1,22 @@
+
+
+#ifndef KERNEL_EXAMPLE_H_
+#define KERNEL_EXAMPLE_H_
+
+
+template<typename Device, typename T>
+struct ExampleFunctor{
+  void operator()(const Device& d, int size, const T* in, T* out);
+};
+
+
+#if GOOGLE_CUDA
+//Partially specialize functor for GpuDevice
+template<typename Eigen::GpuDevice, typename T>
+struct ExampleFunctor{
+  void operator()(const Eigen::GpuDevice& d, int size, const T* in, t* out);
+};
+
+#endif
+
+#endif //KERNEL_EXAMPLE_H_
