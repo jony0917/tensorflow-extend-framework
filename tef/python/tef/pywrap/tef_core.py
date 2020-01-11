49c54751e504573710db3c68d7f21bd61289351b
hehongliang <hehongliang@yidian-inc.com>
2020-01-11 17:15:51 +0800

    add test case for zero_out op

--- tef/python/tef/pywrap/tef_core.py
+++ tef/python/tef/pywrap/tef_core.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
+import os
 
-tef_core = tf.load_op_library('libtef_core.so')
+tef_core = tf.load_op_library(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libtef_core.so'))
+zero_out = tef_core.zero_out
+
+
+
+
--- tef/python/tef/pywrap/tef_core_test.py
+++ tef/python/tef/pywrap/tef_core_test.py
@@ -1,14 +1,18 @@
 # --coding:utf-8--
 import unittest
-import tensorflow as tf
 
+import tensorflow as tf
+import tef_core
 
 class FMModelWarmerTest(unittest.TestCase):
-    def test_zero_out_op(self):
-        tef_core = tf.load_op_library('/github/tensorflow-extend-framework/tef/python/tef/pywrap/libtef_core.so')
-        pass
+    def test_zero_out(self):
 
+        @tf.function
+        def zero_out(x):
+            return tef_core.zero_out(x)
 
+        res = zero_out([1,2,3,4,5,6])
+        print res
 
 if __name__ == '__main__':
     unittest.main()
