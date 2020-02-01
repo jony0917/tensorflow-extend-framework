1bb953c4487f6fdf75210f17800e5fac128aef77
hehongliang <hehongliang@yidian-inc.com>
2020-02-01 19:57:56 +0800

    export ps ops

--- tef/python/tef/pywrap/tef_core.py
+++ tef/python/tef/pywrap/tef_core.py
@@ -4,7 +4,9 @@ import os
 tef_core = tf.load_op_library(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libtef_core.so'))
 zero_out = tef_core.zero_out
 example = tef_core.example
-
-
-
-
+ps_pull = tef_core.ps_pull
+ps_push = tef_core.ps_push
+ps_sparse_pull = tef_core.ps_sparse_pull
+ps_sparse_push = tef_core.ps_sparse_push
+ps_hash_pull = tef_core.ps_hash_pull
+ps_hash_push = tef_core.ps_hash_push
